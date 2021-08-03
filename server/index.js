56a953e2bdedf694a108b69936939187ed8755f3
JAEWOOKJUNG <stich9208@gmail.com>
2021-08-03 21:50:18 +0900

    add: register route 생성 및 post통신 구현 / nodemon 적용

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const port = 3000;
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 
+const { User } = require("./models/User");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
@@ -22,7 +24,18 @@ mongoose
   .catch((err) => console.log("error!", err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!!!!!");
+  res.send("Hello!!!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
